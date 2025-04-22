c6bccfeacbd593145f4c45b4ced9f6499a81af8a
XOK <a.xandr.q@gmail.com>
2025-04-22 16:24:20 +0200

    initial

---
+++ src/app/pages/contacts/contacts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  imports: [],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.scss'
+})
+export class ContactsComponent {
+
+}
