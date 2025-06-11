e5adffb4685c72f85deb855e3a62f44ab91c059c
XOK <a.xandr.q@gmail.com>
2025-06-11 11:08:53 +0200

    remove test title case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('chip');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, chip');
-  });
 });
