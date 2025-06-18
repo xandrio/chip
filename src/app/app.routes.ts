6270eda0dc2038942edbd423393331313ad17214
XOK <a.xandr.q@gmail.com>
2025-06-19 00:28:57 +0200

    correct images and colors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';
 import { StatusComponent } from './pages/status/status.component';
-import { LoginComponent } from './pages/login/login.component';
-import { AdminComponent } from './pages/admin/admin.component';
 import { langRedirectGuard } from './shared/guards/lang-redirect.guard';
 import { LanguageDetectionComponent } from './shell/language-detection/language-detection.component';
 
@@ -15,10 +12,7 @@ export const routes: Routes = [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent, data: { scrollspy: true } },
             { path: 'status', component: StatusComponent },
-            { path: 'login', component: LoginComponent },
-            { path: 'admin', component: AdminComponent }
         ]
     },
     { path: '**', redirectTo: 'en', pathMatch: 'full' } // fallback
-    
 ];
