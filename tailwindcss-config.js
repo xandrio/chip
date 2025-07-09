3c11c31e989d6990a2ec8f28e6fce89f24256e25
Alexandr Ivanov <a.xandr.q@gmail.com>
2025-07-09 03:11:12 +0200

    style: update navigation and language dropdown

--- tailwindcss-config.js
+++ tailwindcss-config.js
@@ -5,10 +5,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
       backgroundImage: {
         'dark-horizontal': 'linear-gradient(to right, #0f1a20, #0f1b21, #0f1c22, #0f1d23, #0f1e24)',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [],};
