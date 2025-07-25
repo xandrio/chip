9c32eeef7377502c49428b4bb72e90a49ef90c80
Alexandr Ivanov <a.xandr.q@gmail.com>
2025-07-25 16:56:11 +0200

    Add simple captcha to contact request form

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -14,18 +14,29 @@ import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angula
 })
 export class ContactsComponent {
   requestForm: FormGroup;
+  captchaA = Math.floor(Math.random() * 10) + 1;
+  captchaB = Math.floor(Math.random() * 10) + 1;
+  captchaError = false;
 
   constructor(private fb: FormBuilder, private contact: ContactService) {
     this.requestForm = this.fb.group({
       name: ['', Validators.required],
       phone: ['', [Validators.required, Validators.pattern(/^[0-9+ ()-]+$/)]],
       model: [''],
-      description: ['']
+      description: [''],
+      captcha: ['', Validators.required]
     });
   }
 
   submitRequest() {
     if (this.requestForm.valid) {
+      const expected = this.captchaA + this.captchaB;
+      const provided = Number(this.requestForm.get('captcha')!.value);
+      if (provided !== expected) {
+        this.captchaError = true;
+        return;
+      }
+      this.captchaError = false;
       this.contact.sendRequest(this.requestForm.value).subscribe({
         next: () => console.log('Request sent'),
         error: err => console.error('Request failed', err)
